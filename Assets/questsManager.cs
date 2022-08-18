b5248a2a36f4bc93dd580ce0f526f7bb531b6d8f
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2022-08-18 14:51:35 +0900

    20220818

--- Assets/questsManager.cs
+++ Assets/questsManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class questsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject popup;
+    public void add_quest()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        popup.SetActive(true);
     }
 }
